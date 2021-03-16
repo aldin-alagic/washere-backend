c675a8c20a1d67baac6dcd98b750910eb66a1db9
fifi98 <bel.filip@hotmail.com>
2021-03-16 13:27:16 +0100

    Add cors middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config.js";
 import express from "express";
 import swaggerJsDoc from "swagger-jsdoc";
 import { serve, setup } from "swagger-ui-express";
+import cors from "cors";
 
 import routes from "./routes/index.js";
 import swaggerOptions from "./config/swagger.js";
@@ -11,6 +12,7 @@ const app = express();
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 if (process.env.NODE_ENV !== "production") app.use("/api-docs", serve, setup(swaggerDocs));
 
+app.use(cors());
 app.use(express.json());
 app.use("/", routes);
 
