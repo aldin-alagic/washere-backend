0d81f7655ecf494cb38a0e0c9f971bc516c8db46
fifi98 <bel.filip@hotmail.com>
2021-03-16 13:25:43 +0100

    Fix swagger configuration

--- config/swagger.js
+++ config/swagger.js
@@ -2,10 +2,9 @@ export default {
   swaggerDefinition: {
     info: {
       title: "WasHere API Documentation",
-      servers: [`http://localhost:${process.env.PORT || 8080}/`],
       version: process.env.npm_package_version,
     },
-    basePath: `${process.env.NODE_ENV === "production" ? "api" : "dev"}.washere.rocks `,
+    basePath: "/",
     securityDefinitions: {
       bearerAuth: {
         type: "apiKey",
