dc41c08112e2dc4bbd36b34691f9a6568519171c
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-03-20 00:02:47 +0100

    Add password reset routes (sending & verifying reset code, resetting password)

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import prisma from "../prisma/client.js";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const register = async (req, res) => {
   try {
@@ -54,4 +57,81 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+const resetCode = async (req, res) => {
+  const email = req.body.email;
+
+  try {
+    const resetCode = Math.floor(100000 + Math.random() * 900000); // broj od 6 znamenki
+
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) {
+      throw "You have entered an invalid e-mail address!";
+    }
+
+    const codeTypeId = (await prisma.code_type.findUnique({ where: { name: "password_reset_code" } })).id;
+
+    // Generate a password reset token
+    const passwordResetCode = await prisma.code.create({
+      data: {
+        code_type_id: codeTypeId,
+        user_id: user.id,
+        value: resetCode.toString(),
+      },
+    });
+
+    const msg = {
+      to: `${req.body.email}`,
+      from: `nibblewashere38@gmail.com`,
+      replyTo: "nibblewashere38@gmail.com",
+      subject: "WasHere - Your password reset code",
+      text: `Hello, you've recently requested a password reset. Please use this code in the application for verification before changing the password: ${resetCode} \n Enjoy!\nSincerely, WasHere team`,
+      html: `<p>Hello, you've recently requested a password reset. Please use this code in the application for verification before changing the password: ${resetCode} <br> Enjoy!<br>Sincerely, WasHere team</p>`,
+    };
+    await sgMail.send(msg);
+    res.status(200).json({ success: true, message: "Reset code has been succesfully sent!" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
+};
+
+const verifyResetCode = async (req, res) => {
+  const { resetCode } = req.body;
+
+  try {
+    const resetCodeDb = await prisma.code.findFirst({ where: { value: resetCode } });
+    if (resetCodeDb == null) throw "You have entered an invalid reset code!";
+
+    res.status(200).json({ success: true, message: "Reset code has been successfully verified!" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  const { password, resetCode } = req.body;
+  try {
+    const passwordResetCode = await prisma.code.findFirst({ where: { value: resetCode } });
+
+    if (passwordResetCode == null) throw "You have entered an invalid reset code!";
+
+    const user = await prisma.user.findUnique({ where: { id: passwordResetCode.user_id } });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.update({
+      where: { id: passwordResetCode.user_id },
+      data: {
+        password: hashedPassword,
+      },
+    });
+
+    await prisma.code.deleteMany({ where: { user_id: passwordResetCode.user_id } });
+
+    res.status(200).json({ success: true, message: "Your password has been successfully reset!" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error });
+  }
+};
+
+export { register, login, resetCode, verifyResetCode, resetPassword };
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router();
 
-import { register, login } from "../controllers/user.js";
+import { register, login, resetCode, verifyResetCode, resetPassword } from "../controllers/user.js";
 
 /**
  * @swagger
@@ -101,5 +101,130 @@ router.post("/", register);
  */
 
 router.post("/login", login);
+
+/**
+ * @swagger
+ * /user/reset-code:
+ *  post:
+ *    tags:
+ *    - "user"
+ *    summary: Request a reset code for password reset process
+ *    parameters:
+ *    - name: "body"
+ *      in: "body"
+ *      description: "User's email"
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *          email:
+ *            type: "string"
+ *    responses:
+ *      '200':
+ *        description: A successful response, denoting that the reset code has been successfully sent to user's email
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *              default: true
+ *            message:
+ *              type: string
+ *              default: Reset code has been succesfully sent!
+ *      '400':
+ *        description: An unsuccesful response
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *              default: false
+ *            message:
+ *              type: string
+ */
+router.post("/reset-code", resetCode);
+
+/**
+ * @swagger
+ * /user/verify-reset-code:
+ *  post:
+ *    tags:
+ *    - "user"
+ *    summary: Verify reset code in password reset process
+ *    parameters:
+ *    - name: "body"
+ *      in: "body"
+ *      description: "Reset code"
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *          resetCode:
+ *            type: "string"
+ *    responses:
+ *      '200':
+ *        description: A successful response, denoting that the reset code has been successfully verified
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *              default: true
+ *            message:
+ *              type: string
+ *              default: Reset code has been succesfully verified!
+ *      '400':
+ *        description: An unsuccesful response
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *              default: false
+ *            message:
+ *              type: string
+ */
+router.post("/verify-reset-code", verifyResetCode);
+
+/**
+ * @swagger
+ * /user/reset-password:
+ *  post:
+ *    tags:
+ *    - "user"
+ *    summary: Reset user's password using reset code and new password
+ *    parameters:
+ *    - name: "body"
+ *      in: "body"
+ *      description: "Reset code"
+ *      schema:
+ *        type: "object"
+ *        properties:
+ *          resetCode:
+ *            type: "string"
+ *          password:
+ *            type: "string"
+ *    responses:
+ *      '200':
+ *        description: A successful response, denoting that the user's password has been successfully reset
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *              default: true
+ *            message:
+ *              type: string
+ *              default: Your password has been successfully reset!
+ *      '400':
+ *        description: An unsuccesful response
+ *        schema:
+ *          type: object
+ *          properties:
+ *            success:
+ *              type: boolean
+ *              default: false
+ *            message:
+ *              type: string
+ */
+router.post("/reset-password", resetPassword);
 
 export default router;
