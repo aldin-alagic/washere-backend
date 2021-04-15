f2c1444f40995a6b20f3534a1eca8fa065ce6f76
fifi98 <filip.bel@hotmail.com>
2021-04-15 13:16:06 +0200

    Add fileUpload Express middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import swaggerJsDoc from "swagger-jsdoc";
 import { serve, setup } from "swagger-ui-express";
 import cors from "cors";
+import fileUpload from "express-fileupload";
 
 import routes from "./routes/index.js";
 import swaggerOptions from "./config/swagger.js";
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV !== "production") app.use("/api-docs", serve, setup(swa
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 app.use("/", routes);
 
 app.listen(process.env.PORT || 8080, () => console.log(`Server started!`));
