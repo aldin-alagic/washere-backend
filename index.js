ca9cbd72de25f3f808e748a444950bdbdb20af13
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-04-21 15:37:31 +0200

    Increase request size limit to 25 mb to allow for multiple images

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const swaggerDocs = swaggerJsDoc(swaggerOptions);
 if (process.env.NODE_ENV !== "production") app.use("/api-docs", serve, setup(swaggerDocs));
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
 app.use(fileUpload());
 app.use("/", routes);
 
