388f71ffb25dd84940440fbb6d4a7e9c41c94082
fifi98 <filip.bel@hotmail.com>
2021-05-06 20:37:58 +0200

    Add jwt authentication for the websocket server

--- config/websocket.js
+++ config/websocket.js
@@ -1,8 +1,20 @@
+import jwt from "jsonwebtoken";
 import { Server } from "socket.io";
 
 export let socket;
 
 export const create = (server) => {
   socket = new Server(server);
+
+  // Check user authentication
+  socket.use((socket, next) => {
+    const token = socket.handshake.auth.token;
+    if (!jwt.verify(token, process.env.JWT_SECRET)) {
+      next(new Error("Authentication error"));
+    } else {
+      next();
+    }
+  });
+
   return socket;
 };
